f70ef97c4adffafec8eb4bcfe59f7c0245bb0a34
Joss <dylajoss@406c8f534be4.ant.amazon.com>
2014-06-19 20:45:03 -0700

    add URL parsing

--- youtube/main.py
+++ youtube/main.py
@@ -2,12 +2,20 @@ import urllib2
 import json # derulo
 import pprint
 
-def retrieve_JSON(video_id):
+
+def get_id(url):
+    """
+    Retrieve the id of a video given its URL
+    url: string
+    """
+    return url.split('v=')[1]
+
+def get_json(video_id):
     """
     Retrieve the JSON object associated with a video
     video_id: string
     """
-    URL = 'http://gdata.youtube.com/feeds/api/videos/' + videoID + '?v=2&alt=jsonc'
+    URL = 'http://gdata.youtube.com/feeds/api/videos/' + video_id + '?v=2&alt=jsonc'
     res = urllib2.urlopen(URL)
 
     return json.load(res)
@@ -17,12 +25,13 @@ def get_category(video_id):
     Return the category of a video given its ID
     video_id: string
     """
-    JSON = retrieve_JSON(video_id)
+    json_object = get_json(video_id)
 
-    return JSON['data']['category']
+    return json_object['data']['category']
 
 
 if __name__ == '__main__':
+    print get_id('https://www.youtube.com/watch?v=IAQ112vC3Qg')
     print get_category('6BTjG-dhf5s')
 
 
