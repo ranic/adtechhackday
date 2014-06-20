59d948f707bb2bd7d0c45ea45783ae037dfd23fe
Joss <dylajoss@406c8f534be4.ant.amazon.com>
2014-06-19 20:39:13 -0700

    some documentation

--- youtube/main.py
+++ youtube/main.py
@@ -2,14 +2,22 @@ import urllib2
 import json # derulo
 import pprint
 
-def retrieve_JSON(videoID):
+def retrieve_JSON(video_id):
+    """
+    Retrieve the JSON object associated with a video
+    video_id: string
+    """
     URL = 'http://gdata.youtube.com/feeds/api/videos/' + videoID + '?v=2&alt=jsonc'
     res = urllib2.urlopen(URL)
 
     return json.load(res)
 
-def get_category(videoID):
-    JSON = retrieve_JSON(videoID)
+def get_category(video_id):
+    """
+    Return the category of a video given its ID
+    video_id: string
+    """
+    JSON = retrieve_JSON(video_id)
 
     return JSON['data']['category']
 
